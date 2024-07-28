badff300e68d00e0aaf0167dc84a8640ad8052ee
Pedro Sartori <pedrosartdr@gmail.com>
2024-07-28 03:10:31 -0300

    Binary Tree Search: insert

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
 #include "headers/print.h"
+
+using std::cout;
+using std::string;
+using std::vector;
 
 template <typename T>
 class TreeNode
@@ -44,6 +49,25 @@ class TreeNode
 
         TreeNode<T>* Right() {return right;}
         TreeNode<T>* Right(TreeNode<T>* obj) {right = obj; return right;}
+
+        // Methods
+        void print() 
+        {
+            print(0);
+        }
+
+        void print(int n)
+        {
+            if(right != nullptr)
+                right->print(n+1);
+
+            for(int i = 0; i < n; i++)
+                cout << "    ";
+            cout << "|->(" << value << ")\n";
+
+            if(left != nullptr)
+                left->print(n+1);
+        }
 };
 
 template<typename T>
@@ -71,14 +95,107 @@ class BinarySearchTree
                 delete root;
         }
 
-        // Getters and Setters
-        TreeNode<T>* Root() {return root;}
-        TreeNode<T>* Root(TreeNode<T>* obj) {root = obj; return root;}
-
         // Methods
         bool isEmpty()
         {
             return root == nullptr;
+        }
+
+        bool contains(TreeNode<T>* node)
+        {
+            if(isEmpty())
+                return false;
+            
+            TreeNode<T>* temp = root;
+            while (temp != nullptr)
+            {
+                if(temp->Value() == node->Value())
+                    return true;
+                else if(node->Value() > temp->Value() && temp->Right() == nullptr)
+                    return false;
+                else if(node->Value() < temp->Value() && temp->Left() == nullptr)
+                    return false;
+                else if(node->Value() > temp->Value()) {
+                    temp = temp->Right();
+                    continue;
+                }
+                else {
+                    temp = temp->Left();
+                    continue;
+                }
+
+                return false;
+            }
+            
+
+        }
+
+        void insert(vector<T> &vect)
+        {
+            for(int i = 0; i < vect.size(); i++)
+            {
+                TreeNode<T>* node = new TreeNode<T>(vect.at(i));
+                if(contains(node))
+                {
+                    delete node;
+                    continue;
+                }
+
+                insert(node);
+            }
+        }
+
+        void insert(TreeNode<T>* node)
+        {
+            if(isEmpty())
+            {
+                root = node;
+                return;
+            }
+
+            TreeNode<T>* temp = root;
+            while(temp != nullptr) 
+            {
+                if(node->Value() == temp->Value())
+                    break;
+
+                if(temp->Left() == nullptr && node->Value() < temp->Value())
+                {
+                    temp->Left(node);
+                    break;
+                }
+
+                if(temp->Right() == nullptr && node->Value() > temp->Value())
+                {
+                    temp->Right(node);
+                    break;
+                }
+
+                if(node->Value() < temp->Value())
+                {
+                    temp = temp->Left();
+                    continue;
+                }
+                else
+                {
+                    temp = temp->Right();
+                    continue;
+                }
+            }
+        }
+
+        void print()
+        {
+            cout << "----------------------------------------\n";
+            cout << "---------  Binary Tree Search  ---------\n";
+            cout << "----------------------------------------\n";
+
+            if(root != nullptr)
+                root->print();
+            else
+                cout << "---------         Empty        ---------\n";
+
+            cout << "----------------------------------------\n";
         }
 
 };
@@ -86,17 +203,11 @@ class BinarySearchTree
 
 int main(int argc, char* argv[])
 {
-    TreeNode<int>* node = new TreeNode<int>(2);
-    node->Left(new TreeNode<int>(1));
-    node->Right(new TreeNode<int>(3));
-
-    print(node->Value());
-    TreeNode<int>* left = node->Left();
-    // delete node;
-    print(left->Value());
-
-    BinarySearchTree<int> bst = BinarySearchTree<int>();
-    print(bst.isEmpty());
+    vector<int> vect = {22, 4, 6, 34, 54, 12, 15, 77, 65, 67, 9, 9, 1, 3, 3, 23, 45, 66};
+    BinarySearchTree<int> tree = BinarySearchTree<int>();
+    tree.insert(vect);
+    tree.print();
+    cout << tree.contains(new TreeNode<int>(71));
 
     return 0;
 }
